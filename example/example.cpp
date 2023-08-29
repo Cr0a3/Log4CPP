94e368dff5712fdfe4adb857655d88dd434e3b89
Toni Graphics <buch.toni@outlook.com>
2023-08-29 15:26:32 +0200

    finishing log4cpp 1.0 + adding example

---
+++ example/example.cpp
@@ -0,0 +1,14 @@
+#define LOG4CPP_IMPL
+#include <Log4CPP/logger.hpp>
+
+int main() {
+    logger Logger;
+
+    Logger.debug("This is a debug message");
+    Logger.info("This is a info message");
+    Logger.sucess("This is a sucess message");
+    Logger.warning("This is a warning message");
+    Logger.error("This is an error message");
+
+    return 0;
+}
--- include/Log4CPP/logger.hpp
+++ include/Log4CPP/logger.hpp
@@ -3,35 +3,59 @@
 
 #include <string>
 
+#define LOG4CPP_IMPL
+
 #ifdef LOG4CPP_IMPL
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+
+#define COLOR_RESET "\033[0m"
+
     class logger {
         private:
-            void log(std::string str) {
+            void log(std::string type, char* typeColor, std::string str, char* strColor) {                
+                auto currentTime = std::chrono::system_clock::now();
+                auto currentTimePoint = std::chrono::system_clock::to_time_t(currentTime);
+                std::tm timeInfo = *std::localtime(&currentTimePoint);
+                std::string space_str = "";
 
+                size_t len = type.length() -1;
+
+
+                for (; len < 8; len++)
+                    space_str += " ";
+                
+                // Format and print the time
+                std::cout   << std::put_time(&timeInfo, "%H:%M:%S")
+                            << " | "
+                            << "\033[1m" << typeColor << type << "\033[21m" COLOR_RESET << space_str
+                            << strColor << str
+                            << COLOR_RESET << std::endl;
             }
-            
+
         public:
             void debug(std::string str) {
-
+                this->log("DEBUG", "\033[39m", str, "");
             }
 
             void info(std::string str) {
-
+                this->log("INFO", "\033[34m", str, "");
             }
 
             void sucess(std::string str) {
-
+                this->log("SUCESS", "\033[32m", str, "");
             }
 
             void warning(std::string str) {
-
+                this->log("WARNING", "\033[35m", str, "");
             }
 
             void error(std::string str) {
-
+                this->log("ERROR", "\033[31m", str, "");
             }
 
-    }
+    };
 #else
     class logger {
         private:
